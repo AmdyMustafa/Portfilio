bf59f6fba8587f9a1b1c0d73f11fd3b6202cb996
tapha221 <amdydaba@gmail.com>
2025-02-22 21:00:16 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Détermine le dossier du modèle en fonction d'une variable d'environnement
+const model = process.env.MODEL || '';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfilio/'
-  
-})
+  base: `/Portfilio/${model}/`, // Définit un chemin dynamique
+});
