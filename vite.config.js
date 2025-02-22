2fc419246db448b8d87374e46462cd0920b175b3
tapha221 <amdydaba@gmail.com>
2025-02-22 21:39:18 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Détermine le dossier du modèle en fonction d'une variable d'environnement
 const model = process.env.MODEL || '';
 
 export default defineConfig({
   plugins: [react()],
-  base: `/Portfilio/${model}/`, // Définit un chemin dynamique
+  base: `/Portfilio/${model}/`, // Chemin de base dynamique
+  build: {
+    outDir: 'build', // Génère les fichiers dans un dossier "build"
+  },
 });
